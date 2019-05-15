be8046ad4f14439ab25d9c41458f9e49efc8b850
Zhuohong Gu <zhuohonggu@10-249-84-55.wireless.oregonstate.edu>
2019-05-14 17:14:17 -0700

    add PyQt visualization

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
-import requests;
+import requests
 import config
+from PyQt5.QtWidgets import *
 
 client_id = config.API_CONFIG['client_id'];
 client_secret = config.API_CONFIG['client_secret'];
@@ -16,11 +17,16 @@ def get_access_token():
     access_token = res['access_token'];
     return access_token;
 
-def get_textbook(access_token):
-    academic_year = input("Enter Academic Year:");
-    term = input("Enter term (Fall, Winter, Spring, Summer):");
-    subject = input("Enter subject (abbreviation):");
-    course_number = input("Enter course number:");
+def get_textbook(access_token,p1,p2,p3,p4):
+    
+    academic_year = str(p1);
+    term = str(p2);
+    subject = str(p3);
+    course_number = str(p4);
+    #academic_year = input("Enter Academic Year:");
+    #term = input("Enter term (Fall, Winter, Spring, Summer):");
+    #subject = input("Enter subject (abbreviation):");
+    #course_number = input("Enter course number:");
 
     url = "https://api.oregonstate.edu/v1/textbooks";
     req = requests.get(url,params={
@@ -32,9 +38,46 @@ def get_textbook(access_token):
         "Authorization":"Bearer " + access_token
     });
     res = req.json();
-    print(res);
+    return str(res['data']);
+
+def visualize():
+    app = QApplication([])
+    window = QWidget();
+    button = QPushButton('Click')
+    text_height = 25;
+    text_year = QTextEdit(placeholderText='year');
+    text_year.setMaximumHeight(text_height);
+    text_term = QTextEdit(placeholderText='term');
+    text_term.setMaximumHeight(text_height);
+    text_subject = QTextEdit(placeholderText='subject');
+    text_subject.setMaximumHeight(text_height);
+    text_number = QTextEdit(placeholderText='number');
+    text_number.setMaximumHeight(text_height);
+    text_result = QTextEdit(placeholderText="result");
+
+    def on_button_clicked():
+        result_text = get_textbook(get_access_token(),text_year.toPlainText(),text_term.toPlainText(),text_subject.toPlainText(),text_number.toPlainText());
+        text_result.setPlainText(result_text);
 
 
-access = get_access_token();
-get_textbook(access);
+    button.clicked.connect(on_button_clicked)
+    
+    vbox = QVBoxLayout();
+    vbox.addWidget(text_year);
+    vbox.addWidget(text_term);
+    vbox.addWidget(text_subject);
+    vbox.addWidget(text_number);
+    vbox.addWidget(button);
+    vbox.addWidget(text_result);
+
+    window.setLayout(vbox);
+
+    window.show();
+
+    app.exec_()
+
+#access = get_access_token();
+#get_textbook(access);
+
+visualize();
 
